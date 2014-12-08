9d2426418e4d0a4e629141caa6015c5af5b89cdd
Sean Smith <seansmith1020@gmail.com>
2014-12-07 18:05:54 -0700

    root has link to new questionnaire form

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root "questionnaires#index"
 
+  resources :questionnaires, only: [:create, :new]
 end
--- spec/features/create_questionnaire_spec.rb
+++ spec/features/create_questionnaire_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
-feature "Questionnaire" do
-  scenario "Creating a new questionnaire" do
+feature "Questionnaires" do
+  scenario "Home page should have link that takes user to a page for creating new questionnaires" do
+    visit '/'
 
+    expect(page).to have_link("New Questionnaire")
+
+    click_link("New Questionnaire")
+
+    expect(page).to have_content("Create New Questionnaire")
   end
 end
