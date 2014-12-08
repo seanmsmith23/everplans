82146e94c534c751eb72dca47c3db0bb55a4c07e
Sean Smith <seansmith1020@gmail.com>
2014-12-08 09:08:22 -0700

    Added presence validation

--- app/models/questionnaire.rb
+++ app/models/questionnaire.rb
@@ -1,7 +1,9 @@
 class Questionnaire < ActiveRecord::Base
   has_many :questions
+  has_many :responses, through: :questions
 
   accepts_nested_attributes_for :questions
 
-  validates :name, :uniqueness => true
+  validates :name, uniqueness: true
+  validates :name, presence: true
 end
